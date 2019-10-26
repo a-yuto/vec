baaee949044302e3c784b11b1e1f39bf649a394b
a-yuto <arakiyutoam@gmail.com>
2019-10-26 10:33:54 +0000

    内積を実装しました

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ fn main() {
     let _x = vec![1.0,5.0];
     let _y = vec![3.0,4.0];
     let _k = 7.0;
-    let _z = Opp::add(&_x,&_y);
-    let _zz = Opp::scl_mul(&_k,&_x);
+    let _z = Opp::in_mul(&_x,&_y);
     println!("{:?}",&_z);
-    println!("{:?}",&_zz);
 }
