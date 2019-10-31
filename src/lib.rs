23f0c4db53fae7f8984a1cbebde8fa074015e64c
a-yuto <arakiyutoam@gmail.com>
2019-10-31 09:32:37 +0000

    計算不可能だった場合エラーを返すようにしました

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 pub struct Opp;
 
 impl Opp {
-    pub fn add(a: &Vec<f64>,b: &Vec<f64>) -> Vec<f64> {
+    pub fn add(a: &Vec<f64>,b: &Vec<f64>) -> Result<Vec<f64>,String>{
         let mut c: Vec<f64> = Vec::new();
         for i in 0..a.len() {
             c.push(a[i] + b[i]);
         }
-        c
+        let _can = a.len() == b.len(); 
+        let d:Result<Vec<f64>,String> = match _can {
+            true  => Ok(c), 
+            false => Err("計算不可能です".to_string()),
+        };
+        d
     }
     
     pub fn scl_mul(k: &f64,a: &Vec<f64>) -> Vec<f64> {
@@ -17,15 +22,21 @@ impl Opp {
         b
     }
     
-    pub fn in_mul(a: &Vec<f64>,b: &Vec<f64>) -> f64 {
+    pub fn in_mul(a: &Vec<f64>,b: &Vec<f64>) -> Result<f64,String> {
         let mut c: f64 = 0.;
         for i in 0..a.len() {
             c += a[i]*b[i];
         }
-        c
+
+        let _can = a.len() == b.len();
+        let d:Result<f64,String> = match _can {
+            true  => Ok(c),
+            false => Err("計算不可能です".to_string()),
+        };
+        d
     }
     //いったん平面ベクトルのみに限って、外積はスカラーを返すようにします
-    pub fn out_mul(a: &Vec<f64>,b: &Vec<f64>) -> f64 {
+    pub fn out_mul(a: &Vec<f64>,b: &Vec<f64>) -> Result<f64,String> {
         let mut c: f64 = 0.;
         for i in 0..a.len() {
             if i%2 == 0 {
@@ -35,7 +46,14 @@ impl Opp {
                 c -= a[i] * b[a.len() - 1 - i]; 
             }
         }
-        c
+
+        let _can = a.len() == b.len();
+        let d:Result<f64,String> = match _can {
+            true  => Ok(c),
+            false => Err("計算不可能です".to_string()),
+        };
+        d
+
     }
 
     pub fn size(a: &Vec<f64>) -> f64 {
@@ -46,13 +64,25 @@ impl Opp {
         b.sqrt()
     }
 
-    pub fn sin_bt_vec(a: &Vec<f64>,b: &Vec<f64>) -> f64 {
-        let c: f64 = Opp::out_mul(&a,&b) / ( Opp::size(&a) * Opp::size(&b) );
-        c
+    pub fn sin_bt_vec(a: &Vec<f64>,b: &Vec<f64>) -> Result<f64,String> {
+        let c: f64 = Opp::out_mul(&a,&b).unwrap() / ( Opp::size(&a) * Opp::size(&b) );
+        
+        let _can = a.len() == b.len();
+        let d:Result<f64,String> = match _can {
+            true  => Ok(c),
+            false => Err("計算不可能です".to_string()),
+        };
+        d
     }
 
-    pub fn cos_bt_vec(a: &Vec<f64>,b: &Vec<f64>) -> f64 {
-        let c: f64 = Opp::in_mul(&a,&b) / ( Opp::size(&a) * Opp::size(&b) );
-        c
+    pub fn cos_bt_vec(a: &Vec<f64>,b: &Vec<f64>) -> Result<f64,String> {
+        let c: f64 = Opp::in_mul(&a,&b).unwrap() / ( Opp::size(&a) * Opp::size(&b) );
+        
+        let _can = a.len() == b.len();
+        let d:Result<f64,String> = match _can {
+            true  => Ok(c),
+            false => Err("計算不可能です".to_string()),
+        };
+        d
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     let _x = vec![1.0,5.0];
     let _y = vec![3.0,4.0];
     let _k = 7.0;
-    let _z = Opp::cos_bt_vec(&_x,&_y);
-    println!("{:?}",&_z);
+    let _z = Opp::add(&_x,&_y);
+    println!("{:?}",&_z.unwrap());
 }
