6e55201eb2c5e86747d681d862556f3f215974aa
a-yuto <arakiyutoam@gmail.com>
2019-10-31 12:30:21 +0000

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub struct Opp;
+extern crate nearly_eq;
+use nearly_eq::*;
 
 impl Opp {
     pub fn add(a: &Vec<f64>,b: &Vec<f64>) -> Result<Vec<f64>,String>{
@@ -84,5 +86,68 @@ impl Opp {
             false => Err("計算不可能です".to_string()),
         };
         d
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn add_works() {
+        let a = vec![1.0,2.0];
+        let b = vec![3.0,4.5];
+        let c = Opp::add(&a,&b).unwrap();
+        assert_eq!(vec![4.0,6.5],c);
+    }
+    #[test]
+    fn scl_ml_works() {
+        let a = vec![1.0,2.0];
+        let b = 5.;
+        let c = Opp::scl_mul(&b,&a);
+        assert_eq!(vec![5.0,10.0],c);
+    }
+    #[test]
+    fn in_mul_works() {
+        let a = vec![1.0,2.0];
+        let b = vec![3.0,4.5];
+        let c = Opp::in_mul(&a,&b).unwrap();
+        let d = vec![1.0,0.0];
+        let e = vec![0.0,1.0];
+        let f = Opp::in_mul(&d,&e).unwrap();
+        assert_eq!(&(12.0 as f64),&c);
+        assert_eq!(&(0.0  as f64),&f);
+    }
+    #[test]
+    fn out_mul_works() {
+        let a = vec![1.0,2.0];
+        let b = vec![3.0,4.5];
+        let c = Opp::out_mul(&a,&b).unwrap();
+        assert_eq!(&(-1.5 as f64),&c);
+    }
+    #[test]
+    fn size_works() {
+        let a = vec![1.0,2.0];
+        let b = Opp::size(&a);
+        let c = (5.0 as f64).sqrt();
+        assert_eq!(c,b);
+    }
+
+    #[test]
+    fn sin_bt_vec_works() {
+        let x = vec![1.0,(3.0 as f64).sqrt()];
+        let y = vec![1.0,0.0];
+        let s = Opp::sin_bt_vec(&y,&x).unwrap();
+        let z = (3.0 as f64).sqrt() / 2.0;
+        assert_nearly_eq!(&s,&z);
+    }
+
+    #[test]
+    fn cos_bt_vec_works() {
+        let x = vec![1.0,(3.0 as f64).sqrt()];
+        let y = vec![1.0,0.0];
+        let s = Opp::cos_bt_vec(&y,&x).unwrap();
+        let z = 0.5 as f64;
+        assert_nearly_eq!(&s,&z);
     }
 }
