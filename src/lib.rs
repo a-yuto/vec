48df895c17aeea117e7d88dc71134db6c67bbec3
a-yuto <arakiyutoam@gmail.com>
2019-11-01 12:14:32 +0000

    moduleを整理しました

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,6 @@ pub struct VecOpp;
 pub struct MatOpp;
 extern crate nearly_eq;
 use nearly_eq::*;
-
-
 impl VecOpp {
     pub fn add(a: &Vec<f64>,b: &Vec<f64>) -> Result<Vec<f64>,String>{
         let mut c: Vec<f64> = Vec::new();
@@ -92,68 +90,6 @@ impl VecOpp {
 }
 
 
-#[cfg(test)]
-mod vec_tests {
-    use super::*;
-    #[test]
-    fn add_works() {
-        let a = vec![1.0,2.0];
-        let b = vec![3.0,4.5];
-        let c = VecOpp::add(&a,&b).unwrap();
-        assert_eq!(vec![4.0,6.5],c);
-    }
-    #[test]
-    fn scl_ml_works() {
-        let a = vec![1.0,2.0];
-        let b = 5.;
-        let c = VecOpp::scl_mul(&b,&a);
-        assert_eq!(vec![5.0,10.0],c);
-    }
-    #[test]
-    fn in_mul_works() {
-        let a = vec![1.0,2.0];
-        let b = vec![3.0,4.5];
-        let c = VecOpp::in_mul(&a,&b).unwrap();
-        let d = vec![1.0,0.0];
-        let e = vec![0.0,1.0];
-        let f = VecOpp::in_mul(&d,&e).unwrap();
-        assert_eq!(&(12.0 as f64),&c);
-        assert_eq!(&(0.0  as f64),&f);
-    }
-    #[test]
-    fn out_mul_works() {
-        let a = vec![1.0,2.0];
-        let b = vec![3.0,4.5];
-        let c = VecOpp::out_mul(&a,&b).unwrap();
-        assert_eq!(&(-1.5 as f64),&c);
-    }
-    #[test]
-    fn size_works() {
-        let a = vec![1.0,2.0];
-        let b = VecOpp::size(&a);
-        let c = (5.0 as f64).sqrt();
-        assert_eq!(c,b);
-    }
-
-    #[test]
-    fn sin_bt_vec_works() {
-        let x = vec![1.0,(3.0 as f64).sqrt()];
-        let y = vec![1.0,0.0];
-        let s = VecOpp::sin_bt_vec(&y,&x).unwrap();
-        let z = (3.0 as f64).sqrt() / 2.0;
-        assert_nearly_eq!(&s,&z);
-    }
-
-    #[test]
-    fn cos_bt_vec_works() {
-        let x = vec![1.0,(3.0 as f64).sqrt()];
-        let y = vec![1.0,0.0];
-        let s = VecOpp::cos_bt_vec(&y,&x).unwrap();
-        let z = 0.5 as f64;
-        assert_nearly_eq!(&s,&z);
-    }
-}
-
 impl MatOpp {
     pub fn add(a: &Vec<Vec<f64>>,b: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
         let _c = vec![VecOpp::add(&a[0],&b[0]).unwrap(),
@@ -169,33 +105,5 @@ impl MatOpp {
         b
     }
     
-}
-mod mat_tests {
-    use super::*;
-
-    #[test]
-    pub fn add_works() {
-        let _a = vec![vec![1.0,2.0],
-                      vec![3.0,4.0]
-        ];
-        let _b = vec![vec![1.0,0.0],
-                      vec![0.0,1.0]
-        ];
-        let _c = vec![vec![0.0,2.0],
-                      vec![3.0,3.0]
-        ];
-        assert_eq!(_a,MatOpp::add(&_b,&_c));
-    }
-    #[test]
-    pub fn scl_mul_works() {
-        let _a = vec![vec![1.0,2.0],
-                      vec![3.0,4.0]
-        ];
-        let _b = vec![vec![2.5,5.0],
-                      vec![7.5,10.0]
-        ];
-        let _k = 2.5;
-        assert_eq!(_b,MatOpp::scl_mul(&_k,&_a));
-    }
 }
 
