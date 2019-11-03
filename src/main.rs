350366ff0aac2868f7dac8099187ee4f1966e894
a-yuto <arakiyutoam@gmail.com>
2019-11-03 11:47:14 +0000

    行列の掛け算を実装しました

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 extern crate vec;
-use vec::VecOpp;
-
+use vec::MatOpp;
 fn main() {
-    let _x = vec![1.0,5.0];
-    let _y = vec![3.0,4.0];
-    let _k = 7.0;
-    let _z = VecOpp::add(&_x,&_y);
-    println!("{:?}",&_z.unwrap());
+
 }
