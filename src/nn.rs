216509923e32b46715ceaebd17c42771e808ab81
a-yuto <arakiyutoam@gmail.com>
2020-01-14 11:10:33 +0000

    nn

--- src/nn.rs
+++ src/nn.rs
@@ -1,2 +1,27 @@
 extern crate matrix;
 use vec::matrix::*;
+
+/*
+ * activity function
+ */
+pub fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + x.exp())    
+}
+pub fn step(x: f64) -> f64 {
+    match x > 0 {
+        true  => 1.0,
+        false => 0.0
+    }
+}
+
+#[cfg(test)]
+mod activity_function_tests {
+    fn sigmoid_work(){
+        assert_eq!(sigmoid(-1),0.26894142137);
+    }
+    fn step_works(){
+         assert_eq!(step(0.0),0.0);
+         assert_eq!(step(5.0),1.0);
+         assert_eq!(step(-100.0),0.0);
+    }
+}
